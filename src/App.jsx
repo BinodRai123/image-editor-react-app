7571342cc4881b4bc036d2042ae4c5e7968b16f3
binod rai <br163406@gmail.com>
2025-12-22 13:22:34 +0545

    initial setup with css folder(normalize.css, layout.css, component.css and index.css) for image editor

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+   return (
+      <div>
+         hello world
+         <h1>hello world</h1>
+         <h1>hello world</h1>
+         <a href="#">focus here</a>
+         <div class="container" style={{ backgroundColor: "red" }}>
+            hello
+         </div>
+      </div>
+   );
+};
+
+export default App;
