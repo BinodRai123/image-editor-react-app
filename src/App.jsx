ef3f4be2c4ffb00e669dd114376e4142bf958505
binod rai <br163406@gmail.com>
2025-12-22 19:43:04 +0545

    added theme.css with css root variable and styled the button of reset , download and choose file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 const App = () => {
    return (
-      <div>
-         hello world
-         <h1>hello world</h1>
-         <h1>hello world</h1>
-         <a href="#">focus here</a>
-         <div class="container" style={{ backgroundColor: "red" }}>
-            hello
-         </div>
-      </div>
+      <>
+         <main>
+            <section className="container">
+               <div class="left">
+                  <div class="left_top row">
+                     <label for="choose image" className="btn" role="button" aria-label="Choose Image">
+                        Choose Image
+                        <input type="file" id="choose image" hidden />
+                     </label>
+                     <button className="btn" role="button" aria-label="Reset">
+                        Reset
+                     </button>
+                     <button className="btn" role="button" aria-label="Download Image">
+                        Download Image
+                     </button>
+                  </div>
+                  <div class="left_bottom"></div>
+               </div>
+            </section>
+         </main>
+      </>
    );
 };
 
