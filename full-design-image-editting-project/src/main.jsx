90cd17150bf703c6c69347091b59a1d6a0838742
binod rai <br163406@gmail.com>
2026-02-14 22:27:10 +0545

    added react reduxt toolkit

--- full-design-image-editting-project/src/main.jsx
+++ full-design-image-editting-project/src/main.jsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./css/index.css";
 import App from "./App.jsx";
 import WrapperFilters from "./WrapFilterData/WrapperFilters.jsx";
+import { store } from "./Redux/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
    <WrapperFilters>
-      <App />
-   </WrapperFilters>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </WrapperFilters>,
 );
